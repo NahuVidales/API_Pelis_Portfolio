fed28e6a2a03e72d60219a4ffd73d90ae745ed5b
Nahuel Vidales <83135275+NahuVidales@users.noreply.github.com>
2023-08-28 01:53:58 -0300

    minusculas

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def get_director(nombre_director: str):
                 costo.append(movies_clean['budget'].values[i])
                 ganancia.append(movies_clean['revenue'].values[i])
     lista_peliculas = {'titulo': titulo, 'fecha_lanzamiento': fecha_lanzamiento, 'retorno': retorno, 'costo': costo, 'ganancia': ganancia}
-    print(f"El director {nombre_director} produjo {peliculas_producidas} peliculas")
+    print(f"El director {nombre_director} produjo {peliculas_producidas} peliculas, con un retorno total de {retorno_total}")
     df_movies = pd.DataFrame(lista_peliculas)
 
     return df_movies
