019a3bf7cd75316afa97ea4e7e13112634b790cc
Nahuel Vidales <83135275+NahuVidales@users.noreply.github.com>
2023-08-29 01:21:17 -0300

    correccion en main

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 from fastapi import FastAPI
 import pandas as pd
 
+#Desarrollo API: Propones disponibilizar los datos de la empresa usando el framework FastAPI. Las consultas que propones son las siguientes:
+#Deben crear 6 funciones para los endpoints que se consumirán en la API, recuerden que deben tener un decorador por cada una (@app.get(‘/’)).
+
 app = FastAPI()
 
 movies_clean = pd.read_csv('movies_dataset_clean.csv')
@@ -11,7 +14,6 @@ movies_clean['spoken_languages'] = movies_clean['spoken_languages'].apply(eval)
 movies_clean['belongs_to_collection'] = movies_clean['belongs_to_collection'].apply(eval)
 movies_clean['revenue'] = movies_clean['revenue'].astype(int)
 movies_clean['production_companies'] = movies_clean['production_companies'].apply(eval)
-
 @app.get('/')
 async def root():
     return {'message': 'Hello World'}
